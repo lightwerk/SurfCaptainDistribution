be810180e463b865933f6f26ed20d45c48a9ff4e
Lars Malach <lp@lightwerk.com>
2014-08-22 16:09:19 +0200

    [TASK] Improves bin/surfRunnerLoop.sh

--- bin/surfRunnerLoop.sh
+++ bin/surfRunnerLoop.sh
@@ -2,10 +2,11 @@
 
 PULLCOUNTDOWN=0;
 
+cd ..;
+
 while true; do
 	((PULLCOUNTDOWN--));
 	if [ $PULLCOUNTDOWN -lt 1 ]; then
-		cd .. && \
 		git fetch --all;
 
 		if [ `git rev-list HEAD...origin/master --count` != 0 ]; then
@@ -22,7 +23,7 @@ while true; do
 		PULLCOUNTDOWN=600;
 	fi
 
-	cd .. && export FLOW_CONTEXT=Production && ./flow surfrunner:deploywaitingfromqueue;
+	export FLOW_CONTEXT=Production && ./flow surfrunner:deploywaitingfromqueue;
 
 	sleep 2;
 done
