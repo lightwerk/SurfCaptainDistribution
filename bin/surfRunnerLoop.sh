3c8683c091547404edb69b82fff34e2366ec1246
Lars Malach <lp@lightwerk.com>
2014-09-08 13:13:22 +0200

    [TASK] Updates lightwerk/surftasks, lightwerk/surfrunner and lightwerk/surfcaptain

--- bin/surfRunnerLoop.sh
+++ bin/surfRunnerLoop.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-PULLCOUNTDOWN=0;
-
 cd ..;
 
 while true; do
-	((PULLCOUNTDOWN--));
-	if [ $PULLCOUNTDOWN -lt 1 ]; then
-		git fetch --all;
-
-		if [ `git rev-list HEAD...origin/master --count` != 0 ]; then
-			git checkout -- . && \
-			git clean -f -f -d && \
-			git pull && \
-			composer install --no-dev --no-interaction && \
-			export FLOW_CONTEXT=Production && \
-			./flow flow:cache:flush --force && \
-			./flow cache:warmup && \
-			./flow doctrine:migrate;
-		fi
-
-		PULLCOUNTDOWN=600;
-	fi
-
 	export FLOW_CONTEXT=Production && ./flow surfrunner:deploywaitingfromqueue;
 
-	sleep 2;
+	sleep 4;
 done
